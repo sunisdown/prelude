5c5c2d6c04af8c5676faf4d157ea14d01d43932d
SunisDown <isdown.sun@gmail.com>
2016-03-01 15:33:34 +0800

    install package

---
+++ utils/init.sh
@@ -0,0 +1,7 @@
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/oracle
+go get -u -v golang.org/x/tools/cmd/gorename
+
+# python
+pip install yapf
